f10fe5c70b42c0d216077384c0049204b403573e
Navdeep Singh <132869610+NavdeepSinghNegi999@users.noreply.github.com>
2024-07-31 17:40:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# import helper
+import helper
 import pickle
 
 model = pickle.load(open(r'model/model.pkl','rb'))
@@ -11,9 +11,8 @@ q1 = st.text_input('Enter question 1')
 q2 = st.text_input('Enter question 2')
 
 if st.button('Find'):
-    # query = helper.PreprocessQuery(q1,q2)
-    # result = model.predict(query)[0]
-    result = 1
+    query = helper.PreprocessQuery(q1,q2)
+    result = model.predict(query)[0]
     
     if result:
         st.header('Duplicate')
